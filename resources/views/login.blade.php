4e95e4df47d295bef3754f425a0a08b5ff0a41d1
Shunmuka <shunmukavalsa@ufl.edu>
2024-07-24 00:27:41 -0400

    seventh commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
 
 @push('scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jqueryvalidation@1.19.5/dist/jquery.validate.js"></script>
+<script src="hhttps://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
 <script>
     $(document).ready(function () {
         $('#loginForm').validate({
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -43,7 +43,7 @@
 
 @push('scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jqueryvalidation@1.19.5/dist/jquery.validate.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
 <script>
     $(document).ready(function () {
         $('#registerForm').validate({
